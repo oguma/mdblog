2f47db334a051af0b2f27d55409cfe5d69912e12
oguma <manabu.oguma@gmail.com>
2019-03-07 01:45:33 +0900

    Update gen.py

--- gen.py
+++ gen.py
@@ -44,18 +44,18 @@ for f in os.listdir('html/tmp'):
     if f not in ['_header.html', '_aside.html', '_footer.html']:
         frag = open('html/tmp/'+f, 'r').read()
         h1 = frag.split('\n', 1)[0]
-        date = ''
-        if re.match("^\d{6}-.+?\.html$", f):
-            y, m, d = '20'+f[0:2], f[2:4], f[4:6]
-            date = '<time datetime="%s">%s</time>' % ('-'.join([y,m,d]), '.'.join([y,str(int(m)),str(int(d))]))
         desc, DESC_NUM = '', 300
         for s in re.sub('<.*?>', '', frag).split('\n')[1:]:
             s = s.strip()
             if len(desc+s) > DESC_NUM:
                 break
             desc += s
+        date = ''
+        if re.match("^\d{6}-.+?\.html$", f):
+            y, m, d = '20'+f[0:2], f[2:4], f[4:6]
+            date = '<time datetime="%s">%s</time>' % ('-'.join([y,m,d]), '.'.join([y,str(int(m)),str(int(d))]))
         post = base.replace('<title>%s</title>' % subtitle, "<title>%s | %s</title>" % (ext(h1), subtitle))
-        post = base.replace('<meta name="description" content="">', '<meta name="description" content="%s">' % desc)
+        post = post.replace('<meta name="description" content="">', '<meta name="description" content="%s">' % desc)
         post = post.replace('<article></article>', "<article>%s%s</article>" % (frag, date))
         open('html/'+f, 'w').write(post)
 
